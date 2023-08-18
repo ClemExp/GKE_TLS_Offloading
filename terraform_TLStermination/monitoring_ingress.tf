f94ee9b7be75fd0822ca8b1f9fe119c8e65e23f8
Clem O Regan <clemoregan@gmail.com>
2023-08-18 19:45:45 +0100

    Add traefik charts

--- terraform_TLStermination/monitoring_ingress.tf
+++ terraform_TLStermination/monitoring_ingress.tf
@@ -2,8 +2,10 @@
 # Following ingress routes are to publish access to monitoring tools through traefik
 resource "kubectl_manifest" "grafana_ingress_route" {
   yaml_body = "${file("../ingress_access/grafana.yaml")}"
+  depends_on = [null_resource.monitoring_stack]
 }
 
 resource "kubectl_manifest" "prometheus_ingress_route" {
   yaml_body = "${file("../ingress_access/prometheus.yaml")}"
+  depends_on = [null_resource.monitoring_stack]
 }
